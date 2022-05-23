a263d5a3f659325ed0b2f735f79e1d431284835f
Marcelo Alves <marcelonralves@gmail.com>
2022-05-23 20:07:07 -0300

    Feature: Rota e Controller

---
+++ app/Http/Controllers/CepController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CepController extends Controller
+{
+    public function index(Request $request, string $cep)
+    {
+       //TODO: Consulta de API e o retorno
+    }
+}
